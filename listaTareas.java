20805f04a93f275b3f436a3997eafd343c6c1fb3
FatimaLDH99 <143623511+FatimaLDH99@users.noreply.github.com>
2023-09-06 08:42:24 -0600

    Add files via upload
    practica de Java lista de tareas. :)

---
+++ listaTareas.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class listaTareas {
+    public static void main(String [] args) {
+       
+        
+    String tarea1 = "poner alarma";
+    String tarea2 = "alimentar al gato ";
+    String tarea3 = "preparar cereal";
+    String tarea4 = "lavar los dientes";
+    String tarea5 = "Sacar las llaves";
+    
+     String cadenaTexto = "Cadena de texto!!! %% $$$";
+     
+     System.out.println( " poner alarma" + tarea1);
+     System.out.println( " alimentar al gato" + tarea2);
+     System.out.println( " poner alarma" + tarea3 );
+     System.out.println( " poner alarma" + tarea4 );
+     System.out.println( " poner alarma" + tarea5 );
+     
+     
+    }
+    
+}
